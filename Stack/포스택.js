29340b6d4304f6317170308630fcc9b13b10a0a6
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2025-03-16 20:52:45 +0900

    포스택

---
+++ Stack/포스택.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+const [N, sequence] = input;
+const sequenceArr = sequence.split(" ").map(Number);
+const stack = [[0], [0], [0], [0]];
+
+for (let i = 0; i < N; i++) {
+    let pushFlag = false;
+    for (let j = 0; j < stack.length; j++) {
+        if (sequenceArr[i] > stack[j][stack[j].length - 1]) {
+            stack[j].push(sequenceArr[i]);
+            pushFlag = true;
+            break;
+        }     
+    }
+    if (!pushFlag) {
+        console.log("NO");
+        return;
+    }
+}
+
+console.log("YES")
